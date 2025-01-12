e551ed83a2a40e0e550b877908f23303994c935b
Mikey B <michael.bell1990@gmail.com>
2025-01-12 01:33:43 +0000

    adding origional-ps-try

---
+++ 12-days-of-christmas.ps1
@@ -0,0 +1,5 @@
+#1-10-25
+$e='A Partridge in a Pear Tree.0Two Turtle Doves, and0Three French Hens,0Four Calling Birds,0Five Gold Rings,0Six Geese-a-Lay3Seven Swans-a-Swimm3Eight Maids-a-Milk3Nine Ladies Danc3Ten Lords-a-Leap3Eleven Pipers Pip3Twelve Drummers Drumm3'-replace3,'ing,0'-split0
+echo First Second Third Fourth Fifth Sixth Seventh Eighth Ninth Tenth Eleventh Twelfth|%{"On the $_ day of Christmas
+My true love sent to me"
+$q++..0|%{$e[$_]};''}
